116f01df5bbfe6f6aa8561c2f7248a653fc86e67
asus PC <pedroaccruz@gmail.com>
2024-09-07 15:07:30 -0300

    readme+lol

---
+++ README.md
@@ -0,0 +1,3 @@
+I'm a Linux User, but when I want to play something I use windows. 
+
+Here's a collection of scripts that I find useful for my use cases <3
---
+++ lol.ps1
@@ -0,0 +1,10 @@
+
+Add-Type -AssemblyName System.Windows.Forms
+
+# here you put your password
+$password = ""
+
+[System.Windows.Forms.Clipboard]::SetText($password)
+
+
+
